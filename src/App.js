8adaa6246357c84de115cafdad79e2b1850985f6
Stas Arshanski <stuk88@gmail.com>
2023-11-18 21:29:43 +0200

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+const websiteList = ['fo1.altius.finance', 'fo2.altius.finance'];
+
+const CredentialForm = () => {
+  const [selectedWebsite, setSelectedWebsite] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+  const [token, setToken] = useState('');
+
+  const handleWebsiteChange = (event) => {
+    setSelectedWebsite(event.target.value);
+  };
+
+  const handleUsernameChange = (event) => {
+    setUsername(event.target.value);
+  };
+
+  const handlePasswordChange = (event) => {
+    setPassword(event.target.value);
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    // Assuming you have an API endpoint to send credentials
+    const apiUrl = 'http://127.0.0.1:8000/api/crawl/login';
+
+    try {
+      const response = await fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          website: selectedWebsite,
+          username,
+          password,
+        }),
+      });
+
+      if (response.status === 200) {
+        // Successful response
+        const data = await response.json();
+        setToken(data.token);
+        setErrorMessage('');
+      } else if (response.status === 403) {
+        // Credentials not correct
+        setToken('');
+        setErrorMessage('Credentials not correct');
+      } else {
+        // Handle other status codes as needed
+        setToken('');
+        setErrorMessage(`Error: ${response.status}`);
+      }
+    } catch (error) {
+      console.error('Error sending credentials:', error);
+      setToken('');
+      setErrorMessage('Error sending credentials');
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Credential Form</h1>
+      <form onSubmit={handleSubmit}>
+        <label>
+          Select a website:
+          <select value={selectedWebsite} onChange={handleWebsiteChange}>
+            <option value="">Select a website</option>
+            {websiteList.map((website) => (
+              <option key={website} value={website}>
+                {website}
+              </option>
+            ))}
+          </select>
+        </label>
+        <br />
+        <label>
+          Username:
+          <input type="text" value={username} onChange={handleUsernameChange} />
+        </label>
+        <br />
+        <label>
+          Password:
+          <input type="password" value={password} onChange={handlePasswordChange} />
+        </label>
+        <br />
+        <button type="submit">Send</button>
+      </form>
+      {errorMessage && <p style={{ color: 'red' }}>{errorMessage}</p>}
+      {token && <p style={{ color: 'green' }}>Token: {token}</p>}
     </div>
   );
-}
+};
 
-export default App;
+export default CredentialForm;
